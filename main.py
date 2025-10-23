d10aeb03161e616a053dae29f9371ea7df7eecc7
Juanpabloflorez <juflorezru@unal.edu.co>
2025-10-23 15:53:49 -0500

    Add Animal classes with methods for behavior

---
+++ main.py
@@ -0,0 +1,64 @@
+#Autor: Juan Pablo Florez Rubio
+
+class Animal:
+
+    def __init__(self, especie, edad):
+
+        self.especie = especie
+        self.edad = edad
+
+        def hablar(self):
+            pass
+
+        def moverse(self):
+            pass
+
+        def describeme(self):
+            pass
+
+class BiomasaPanspermica(Animal):
+
+    def hablar(self):
+        print("*Se comunica con cambios en su temperatura corporal*")
+
+    def moverse(self):
+        print("Usa ventosas para jalar el suelo")
+
+    def describeme(self):
+        print("Hola, salí de un meteoro para reproducir mi especie en este planeta para preservar mi especie")
+
+    def reproducir(self):
+        print("Expulsa esporas que crecen rapidamente con la luz proporcionada por una estrella amarilla")
+
+class EstrellaDeMar(Animal):
+    def hablar(self):
+        print("Expulsa burbujas")
+
+    def moverse(self):
+        print("usa cambios de presion en el agua")
+
+    def describeme(self):
+        print("Hola, soy una estrella de mar, tengo simetria pentalateral y consumo crustaceos pequenos")
+
+    def comer(self):
+        print("Disuelvo organismos pequenos con enzimas en mi superficie")
+
+class Blob(Animal):
+    def hablar(self):
+        print("Hace sonidos de ultra baja frecuencia")
+
+    def moverse(self):
+        print("Usa sus aletas gigantes para desplazarse")
+
+    def describeme(self):
+        print("Hola, soy una especie conocida unicamente por su sonido, se presume que soy el animal mas grande del mundo")
+
+    def AccionTerrorifica(self):
+        print("Soy capaz de hundir cualquier embarcacion y mi capacidad mental me permite competir con el hombre intelectualmente")
+
+def main():
+    especieDesconocida = BiomasaPanspermica("Desconocido", 1539)
+    especieDesconocida.describeme()
+
+if __name__ == "__main__":
+    main()
